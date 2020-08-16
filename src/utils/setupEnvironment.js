a2fc532d7839098c580c5f3f73d6e40aa22d9b9f
Bohdan Petryshyn <b.y.petryshyn@gmail.com>
2020-08-16 19:27:38 +0300

    Remove setupEnvironment error handling to prevent silent fails

--- src/utils/setupEnvironment.js
+++ src/utils/setupEnvironment.js
@@ -3,15 +3,11 @@ const logger = require('./logger');
 const mkdirRecursive = require('./mkdirRecursive');
 
 module.exports = () =>
-  Promise.all([mkdirRecursive(RESULTS_PATH), mkdirRecursive(BUFFER_PATH)])
-    .then(() =>
-      logger.log(
-        `Results path ${RESULTS_PATH}, buffer path ${BUFFER_PATH} created successfully.`
-      )
+  Promise.all([
+    mkdirRecursive(RESULTS_PATH),
+    mkdirRecursive(BUFFER_PATH),
+  ]).then(() =>
+    logger.info(
+      `Results path ${RESULTS_PATH}, buffer path ${BUFFER_PATH} created successfully.`
     )
-    .catch(error =>
-      logger.error(
-        { error },
-        `Failed to create results path ${RESULTS_PATH}, buffer path ${BUFFER_PATH}.`
-      )
-    );
+  );
